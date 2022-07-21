2d588df1a5a303d11c8d8f2181463ca6d631e48e
Flavio Nobre <flavionobredev@gmail.com>
2022-07-21 16:38:22 -0300

    refactor: create func to verify value

--- alura/curso-python--comecando-com-a-linguagem/001-adivinhacao.py
+++ alura/curso-python--comecando-com-a-linguagem/001-adivinhacao.py
@@ -1,14 +1,31 @@
-print("===================")
+MAX_ATTEMPS = 3
+SECRET_NUMBER = 42
+
+def check_if_correct(number: int, correct_number: int) -> bool:
+	if(number == correct_number):
+		print("Parabéns, você acertou! O numero secreto é: ", correct_number)
+		return True
+	elif(number > correct_number):
+		print("Quase! Você informou um número maior.")
+	elif(number < correct_number):
+		print("Quase! Você informou um número menor.")
+	
+	return False
+
+
+print("\n===================")
 print("Bem vindo no jogo de Adivinhação")
-print("===================")
+print("===================\n")
 
-secret_number = 42
 
-attemp = int(input("Digite o seu numero: "))
-
-if(attemp == secret_number):
-    print("Parabéns, você acertou! O numero secreto é: ", secret_number)
-elif(attemp > secret_number):
-    print("Quase! Você informou um número maior.")
-elif(attemp < secret_number):
-    print("Quase! Você informou um número menor.")
+current_attemp = 0
+while(current_attemp < MAX_ATTEMPS):
+	attemp = int(input("\nDigite o seu numero: "))
+	current_attemp+=1
+	result = check_if_correct(attemp, SECRET_NUMBER)
+	if(result):
+		break
+	elif(current_attemp < MAX_ATTEMPS):
+		print(f"Você tem mais {MAX_ATTEMPS - current_attemp} tentativas")
+else:
+	print("Tentativas excedidas\nNão desista! Tente novamente.")
