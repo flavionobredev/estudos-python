85366cd7179291ed0585a40065070f0b4bed5d3d
Flavio Nobre <flavionobredev@gmail.com>
2022-07-21 13:26:30 -0300

    feat: improves example 001

--- alura/curso-python--comecando-com-a-linguagem/001-adivinhacao.py
+++ alura/curso-python--comecando-com-a-linguagem/001-adivinhacao.py
@@ -7,6 +7,8 @@ secret_number = 42
 attemp = int(input("Digite o seu numero: "))
 
 if(attemp == secret_number):
-    print("Parabéns, você acertou!")
-else:
-    print("Você errou, o número secreto era {}".format(secret_number))
+    print("Parabéns, você acertou! O numero secreto é: ", secret_number)
+elif(attemp > secret_number):
+    print("Quase! Você informou um número maior.")
+elif(attemp < secret_number):
+    print("Quase! Você informou um número menor.")
